129c8ca180c51dec84e96c71adc1e66cc63c7f80
nauzete <nauzet928@gmail.com>
2026-08-17 00:55:37 +0100

    Add LAME development dependency

--- tools/build-bitedj-arm64.sh
+++ tools/build-bitedj-arm64.sh
@@ -28,6 +28,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
     libportmidi-dev libsndio-dev \
     libavcodec-dev libavformat-dev libavutil-dev libswresample-dev \
     libchromaprint-dev libfftw3-dev libflac-dev libid3tag0-dev libmad0-dev \
+    libmp3lame-dev \
     libmodplug-dev libopus-dev libopusfile-dev libsndfile1-dev \
     libshout3-dev libtag1-dev libvorbis-dev libwavpack-dev \
     libebur128-dev librubberband-dev libsoundtouch-dev \
